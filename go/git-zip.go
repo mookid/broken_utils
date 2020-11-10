225ccd92da94f281e64fdd84fb0acf1cbec2f596
Nathan Moreau <nathan.moreau@m4x.org>
2020-11-10 01:09:29 +0100

    git-zip: cleanup.

--- go/git-zip.go
+++ go/git-zip.go
@@ -11,6 +11,11 @@ import (
 	"os/exec"
 )
 
+type r struct {
+	data    []byte
+	relpath string
+}
+
 func usage() {
 	fmt.Printf(
 		`git zip [-o archive-name]
@@ -24,47 +29,83 @@ Options:
 	os.Exit(2)
 }
 
-func copyfile(w *zip.Writer, relpath string) error {
+func readFile(relpath string) (result *r, err error) {
 	file, err := os.Open(relpath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer file.Close()
 
-	f, err := w.Create(relpath)
+	data, err := ioutil.ReadAll(file)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	_, err = io.Copy(f, file)
-	return err
+
+	return &r{data, relpath}, nil
 }
 
-func main() {
-	out := flag.String("o", "repo.zip", "")
-	flag.Usage = usage
-	flag.Parse()
-
+func readPaths() (relpaths []string) {
 	files, err := exec.Command("git", "ls-files", "-z").Output()
 	if err != nil {
 		fmt.Printf("not in a git repository\n")
 		os.Exit(2)
 	}
 
-	var buf bytes.Buffer
-	w := zip.NewWriter(&buf)
 	hi := 0
 	for lo := 0; lo < len(files); lo = hi + 1 {
 		for hi = lo; hi < len(files) && files[hi] != 0; hi++ {
 		}
-		if err := copyfile(w, string(files[lo:hi])); err != nil {
-			fmt.Printf("error creating the archive: %s\n", err.Error())
-			os.Exit(2)
+		relpaths = append(relpaths, string(files[lo:hi]))
+	}
+	return relpaths
+}
+
+func readFiles(relpaths []string) (results []r) {
+	ok := true
+	results = make([]r, len(relpaths))
+	for i, relpath := range relpaths {
+		result, err := readFile(relpath)
+		if err != nil {
+			ok = false
+			fmt.Printf("%v\n", err)
+		}
+		results[i] = *result
+	}
+	if !ok {
+		os.Exit(2)
+	}
+	return results
+}
+
+func createArchive(results []r) []byte {
+	var buf bytes.Buffer
+	w := zip.NewWriter(&buf)
+	for _, result := range results {
+		f, err := w.Create(result.relpath)
+		if err == nil {
+			_, err = io.Copy(f, bytes.NewReader(result.data))
+		}
+		if err != nil {
+			fmt.Printf("error creating the archive: %v\n", err)
 		}
 	}
 	w.Close()
+	return buf.Bytes()
+}
 
-	if err := ioutil.WriteFile(*out, buf.Bytes(), 0644); err != nil {
-		fmt.Printf("error while writing the archive: %s %s\n", *out, err.Error())
+func writeArchive(data []byte, out string) {
+	if err := ioutil.WriteFile(out, data, 0644); err != nil {
+		fmt.Printf("error while writing the archive: %s %v\n", out, err)
 		os.Exit(2)
 	}
+}
+
+func main() {
+	out := flag.String("o", "repo.zip", "")
+	flag.Usage = usage
+	flag.Parse()
+	relpaths := readPaths()
+	results := readFiles(relpaths)
+	data := createArchive(results)
+	writeArchive(data, *out)
 }
