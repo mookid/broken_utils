77521a370e7ece2ba91f4e205ff03a47d0faaee2
Nathan Moreau <nathan.moreau@m4x.org>
2020-12-03 19:00:03 +0100

    d: new program.

---
+++ go/d.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func exit_like_child(err error) {
+	exiterr, ok := err.(*exec.ExitError)
+	if ok {
+		os.Exit(exiterr.ExitCode())
+	}
+	if err != nil {
+		os.Exit(2)
+	}
+}
+
+func usage() {
+	fmt.Println(`d [-c] [arg...]
+Forward to git diff.
+OPTIONS:
+  -c                  View staged changes (--cached)
+  args                Other args are forwarded to git diff`)
+	os.Exit(2)
+}
+
+func main() {
+	args := []string{"diff", "--patch-with-stat", "--stat-width=1000"}
+	for _, arg := range os.Args[1:] {
+		switch arg {
+		case "-c":
+			arg = "--cached"
+		case "-h", "--help":
+			usage()
+		}
+		args = append(args, arg)
+	}
+	cmd := exec.Command("git", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	exit_like_child(cmd.Run())
+}
