07953848983750286d54f66e4b766466ee9838be
Nathan Moreau <nathan.moreau@m4x.org>
2020-12-31 10:59:12 +0100

    gr: with multiple files

--- go/gr.go
+++ go/gr.go
@@ -10,10 +10,25 @@ import (
 	"github.com/fatih/color"
 )
 
-func main() {
-	r := bufio.NewReader(os.Stdin)
-	groups := make(map[string][]string)
-	b := color.New(color.FgCyan).SprintFunc()
+var (
+	groups map[string][]string = make(map[string][]string)
+)
+
+func readFile(filename string) {
+	var (
+		f   io.Reader
+		err error
+	)
+	if filename == "-" {
+		f = os.Stdin
+	} else {
+		f, err = os.Open(filename)
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read file %s: %v\n", filename, err)
+	}
+
+	r := bufio.NewReader(f)
 	for {
 		line, err := r.ReadString('\n')
 		if err != nil {
@@ -30,6 +45,18 @@ func main() {
 
 		groups[parts[0]] = append(groups[parts[0]], parts[1])
 	}
+}
+
+func main() {
+	fromStdin := true
+	for _, arg := range os.Args[1:] {
+		fromStdin = false
+		readFile(arg)
+	}
+	if fromStdin {
+		readFile("-")
+	}
+	b := color.New(color.FgCyan).SprintFunc()
 	for filename, results := range groups {
 		fmt.Println(b(filename))
 		for _, result := range results {
