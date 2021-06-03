3e110d63bf2bd88332a8af95ef9171b12738d012
Nathan Moreau <nathan.moreau@m4x.org>
2021-06-03 13:29:19 +0200

    d: add -v option to exclude directory from the diff.

--- go/d/d.go
+++ go/d/d.go
@@ -19,12 +19,13 @@ func exitLikeChild(err error) {
 }
 
 func usage() {
-	fmt.Println(`d [-c] [arg...]
+	fmt.Println(`d [-c] [-m] [-o] [-v DIR] [arg...]
 Forward to git diff.
 OPTIONS:
   -c                  View staged changes (--cached)
   -m                  View diff from origin/master
   -o                  List changed files only (--name-only)
+  -v DIR              Exclude the directory from the diff
   args                Other args are forwarded to git diff`)
 	os.Exit(2)
 }
@@ -44,25 +45,40 @@ func inferMainBranchName() string {
 }
 
 func main() {
-	args := []string{"diff", "--patch-with-stat", "--stat-width=1000"}
-	for _, arg := range os.Args[1:] {
-		switch arg {
+	argsLists := [][]string{{"diff", "--patch-with-stat", "--stat-width=1000"}, {}}
+	for i := 1; i < len(os.Args); i++ {
+		arg0 := os.Args[i]
+		arg1 := ""
+		switch arg0 {
 		case "-c":
-			arg = "--cached"
+			arg0 = "--cached"
 		case "-m":
-			arg = inferMainBranchName() + "..."
+			arg0 = inferMainBranchName() + "..."
 		case "-o":
-			arg = "--name-only"
+			arg0 = "--name-only"
+		case "-v":
+			i++
+			arg0 = ""
+			arg1 = fmt.Sprintf(":!%s", os.Args[i])
 		case "-h", "--help":
 			usage()
 		}
-		if strings.HasPrefix(arg, "-") {
-			i, err := strconv.Atoi(arg[1:])
+		if strings.HasPrefix(arg0, "-") {
+			i, err := strconv.Atoi(arg0[1:])
 			if err == nil && i > 0 {
-				arg = fmt.Sprintf("HEAD~%d..", i)
+				arg0 = fmt.Sprintf("HEAD~%d..", i)
 			}
 		}
-		args = append(args, arg)
+		argsLists[0] = append(argsLists[0], arg0)
+		argsLists[1] = append(argsLists[1], arg1)
+	}
+	var args []string
+	for _, argsList := range argsLists {
+		for _, arg := range argsList {
+			if arg != "" {
+				args = append(args, arg)
+			}
+		}
 	}
 	cmd := exec.Command("git", args...)
 	cmd.Stdout = os.Stdout
